ce60139ae625ac60b675eefc3c049cac699363a0
PulkitMadan <pulkit.madan@ontariotechu.net>
2022-04-03 21:59:51 -0400

    fintuning classification layers

--- src/train.py
+++ src/train.py
@@ -14,7 +14,7 @@ from torchinfo import summary
 
 from sklearn.metrics import classification_report
 
-from pytorch_pretrained_vit import ViT
+# from pytorch_pretrained_vit import ViT
 #import local classes/functions
 from models.coatnet import coatnet_0
 from models.convnext import convnext_small
@@ -56,7 +56,7 @@ def add_args(parser):
         "--model",
         default='resnet',
         type=str,
-        help="Model name",
+        help="Model name; available models: {resnet, vit_16, vit_32, convnext, coatnet}",
     )
     parser.add_argument(
         "--pretrain",
@@ -89,14 +89,21 @@ def main(args):
         # Set the size of each output sample to class_size
         net.fc = nn.Linear(net.fc.in_features, class_size)
     #using torch vision (v. 0.12.0+)
-    elif args.model =='vit_tv':
+    elif args.model =='vit_16_tv':
+        net = models.vit_b_16(pretrained=args.pretrain)
+        net.heads.head = nn.Linear(in_features=net.heads.head.in_features, out_features=class_size, bias=True)
+    elif args.model =='vit_32_tv':
         net = models.vit_b_32(pretrained=args.pretrain)
         net.heads.head = nn.Linear(in_features=net.heads.head.in_features, out_features=class_size, bias=True)
     elif args.model =='convnext_tv':
         net = models.convnext_small(pretrained=args.pretrain)
         net.classifier[2] = nn.Linear(in_features=net.classifier[2].in_features, out_features=class_size, bias=True)
     #using local
-    elif args.model =='vit':
+    # TODO: check if vit_16 works
+    elif args.model =='vit_16':
+        net = ViT('B_16', pretrained=args.pretrain)
+        net.fc = nn.Linear(in_features=net.fc.in_features, out_features=class_size, bias=True)
+    elif args.model =='vit_32':
         net = ViT('B_32', pretrained=args.pretrain)
         net.fc = nn.Linear(in_features=net.fc.in_features, out_features=class_size, bias=True)
     elif args.model =='convnext':
@@ -112,9 +119,19 @@ def main(args):
         #load original resnet models here
         net = load_model(args.model)
         net.module.fc = nn.Linear(in_features=net.module.fc.in_features, out_features=class_size, bias=True)
- 
+    
     print(f'Training on {args.model}')
     print(net)
+    
+    trainable_params = 0 
+    if args.pretrain: 
+        for name, param in net.named_parameters(): 
+            if not 'fc' in name: 
+                param.requires_grad = False 
+            else: 
+                trainable_params += param.flatten().size()[0]
+                
+    print(f'{trainable_params=}')
 
     # Load model from save to scratch, if granted and exist
     model_name = args.model
