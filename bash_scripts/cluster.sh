7a4a751037c2bfd5aa8991d9a78803194b42f0b7
AxelBogos <bogosaxel@gmail.com>
2022-05-01 10:24:50 -0400

    put back helper and test bash file

--- bash_scripts/cluster.sh
+++ bash_scripts/cluster.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=convatt                                  # Job name
+#SBATCH --job-name=refactoring_test                                  # Job name
 #SBATCH --time=1-23:00:00                                     # time limit
 #SBATCH --cpus-per-task=4                                   # Ask for 4 CPUs
 #SBATCH --mem=16Gb                                           # Ask for 16 GB of RAM
 #SBATCH --gres=gpu                                          # Ask for GPU
-#SBATCH --output=/scratch/jizhouw/logs/slurm-%j-%x.out   # log file
-#SBATCH --error=/scratch/jizhouw/logs/slurm-%j-%x.error  # log file
+#SBATCH --output=/scratch/axelbogos/logs/slurm-%j-%x.out   # log file
+#SBATCH --error=/scratch/axelbogos/logs/slurm-%j-%x.error  # log file
 
 # Arguments
 # $1: Path to code directory
 # Git clone repo
-git clone https://github.com/PulkitMadan/convolution-vs-attention.git
+git clone -b refactoring_tests https://github.com/PulkitMadan/convolution-vs-attention.git
 
 # Copy code dir to snapshot code dir (rsync outside of compute node/remember to execute if updated code)
 rsync --bwlimit=10mb -av convolution-vs-attention ~/scratch/code-snapshots/ --exclude .git 
-
+rm -r convolution-vs-attention
 # Copy snapshot code dir to the compute node and cd there
 rsync -av --relative "$1" $SLURM_TMPDIR --exclude ".git"
 # Copy data to SLURM dir (done once)
@@ -25,16 +25,11 @@ cd $SLURM_TMPDIR/"$1/src"
 module purge
 module load StdEnv/2020
 module load python/3.9.6
-#loading opencv module changes python/3.9.6 => python/3.8.10
-#module load opencv/4.5.1
-#module load pytorch/1.8.1
 export PYTHONUNBUFFERED=1
 virtualenv $SLURM_TMPDIR/venv
 source $SLURM_TMPDIR/venv/bin/activate
 python -m pip install --upgrade pip
-#python -m pip install numpy pandas scikit-learn
 python -m pip install -r ../requirements_cluster.txt
-#pip3 install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
 
 # Prints
 echo "Currently using:"
@@ -55,7 +50,7 @@ wandb login $WANDB_API_KEY
 # Run Script
 # training
 # --name => Name of your Wandb experiment.
-python train.py --train --model convnext --pretrain --name ABC1 #--load
+python train.py --train --model convnext --pretrain --name refactored_resnet_test_rub #--load
 # testing
 python train.py --model resnet --pretrain --load
 
